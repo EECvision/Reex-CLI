d126bdf9b8b95cf727412c7ed99d942ece087c93
EECvision <eec.studies@gmail.com>
2026-02-08 17:13:00 +0100

    update react-query helper function

--- services/hook-service.js
+++ services/hook-service.js
@@ -269,37 +269,56 @@ ${hooks.join("\n\n")}
   generateIndexFile(generatedDir, exportLines) {
     const rawContent = `// Generated file - DO NOT EDIT
 import {
-  QueryKey,
+  type QueryKey,
+  type UseMutationOptions,
+  type UseQueryOptions,
+  type UseQueryResult,
+  type DefaultError,
   useMutation,
   useQuery,
-  UseMutationOptions,
-  UseQueryOptions,
 } from "@tanstack/react-query";
 
-export const useApiMutation = <TData, TVariables>(
+// 1. Mutation Wrapper
+export const useApiMutation = <
+  TData = unknown,
+  TVariables = void,
+  TError = DefaultError,
+  TContext = unknown,
+>(
   mutationFn: (variables: TVariables) => Promise<TData>,
-  options?: UseMutationOptions<TData, unknown, TVariables>
-) =>
-  useMutation({
+  options?: Omit<
+    UseMutationOptions<TData, TError, TVariables, TContext>,
+    "mutationFn"
+  >,
+) => {
+  return useMutation<TData, TError, TVariables, TContext>({
     mutationFn,
     ...options,
   });
+};
 
-export const useApiQuery = <TData>(
-  queryKey: QueryKey,
-  queryFn: () => Promise<TData>,
+// 2. Query Wrapper
+export const useApiQuery = <
+  TQueryFnData = unknown,
+  TError = DefaultError,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+>(
+  queryKey: TQueryKey,
+  queryFn: () => Promise<TQueryFnData>,
   options?: Omit<
-    UseQueryOptions<TData, unknown, TData, QueryKey>,
+    UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
     "queryKey" | "queryFn"
-  >
-) =>
-  useQuery({
+  >,
+): UseQueryResult<TData, TError> => {
+  return useQuery<TQueryFnData, TError, TData, TQueryKey>({
     queryKey,
     queryFn,
     refetchOnWindowFocus: false,
     retry: 1,
     ...options,
   });
+};
 
 ${exportLines.join("\n")}
 `;
